531a900f3ba2a0afc01690c61c6ea873076cfb42
mosheurpau <mosheur.pau@gamil.com>
2022-03-29 23:47:49 +0600

    added Responsive pricing card with Tailwind and Hero icons

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -3,9 +3,42 @@ import PricingOption from "../PricingOption/PricingOption";
 
 const Pricing = () => {
   const pricingOptions = [
-    { id: 1, name: "Free", price: 0 },
-    { id: 2, name: "Regular", price: 9.99 },
-    { id: 3, name: "Premium", price: 19.99 },
+    {
+      id: 1,
+      name: "Free",
+      price: 0,
+      benefits: [
+        "lifetime free",
+        "unlimited deals",
+        "localized deals",
+        "fantastic deals",
+        "crazy deals",
+      ],
+    },
+    {
+      id: 2,
+      name: "Regular",
+      price: 9.99,
+      benefits: [
+        "lifetime Yes",
+        "unlimited deals",
+        "localized deals",
+        "fantastic deals",
+        "crazy deals",
+      ],
+    },
+    {
+      id: 3,
+      name: "Premium",
+      price: 19.99,
+      benefits: [
+        "lifetime No",
+        "unlimited deals",
+        "localized deals",
+        "fantastic deals",
+        "crazy deals",
+      ],
+    },
   ];
   return (
     <div className="bg-gray-300 p-4 mt-8">
