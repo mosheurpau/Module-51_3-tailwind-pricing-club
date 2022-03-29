4f8c50bb1b07bea2f90df40599b2d4a8bbf965ea
mosheurpau <mosheur.pau@gamil.com>
2022-03-29 22:32:27 +0600

    added Responsive Navbar using react and tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my pricing club.</h1>
+      <Navbar></Navbar>
+      <h1 className="text-6xl">Welcome to my pricing club.</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-4xl">Save most of your money</h2>
+      <p>By giving your money to me!!!</p>
+    </div>
+  );
+};
+
+export default Header;
